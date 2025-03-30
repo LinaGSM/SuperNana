c4e087e0e4d02da421dd6074d616408a36ab6211
capatainkomic <magiqueram@gmail.com>
2025-03-30 23:14:19 +0200

    Message Archiving System

--- src/main/java/demo/controller/ArchivedMessageController.java
+++ src/main/java/demo/controller/ArchivedMessageController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/archived-messages")
+@RequestMapping("/archive")
 public class ArchivedMessageController {
 
     @Autowired
